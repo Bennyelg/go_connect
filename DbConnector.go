75ddd592d154d20e2e3651f287be568298742559
Bennyelg <Benjaminelgazar@gmail.com>
2016-12-09 18:51:39 +0200

    Simple usage added postgres

--- DbConnector.go
+++ DbConnector.go
@@ -6,9 +6,12 @@ import (
 	"os"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	"strconv"
 )
+
 type Database struct {
+	dbType string
 	username string
 	password string
 	port string
@@ -33,16 +36,21 @@ func (db *Database) GetDatabaseDetails(env string) {
 	username := infoTree.Get("user")
 	password := infoTree.Get("password")
 	port := infoTree.Get("port")
+	database := infoTree.Get("database")
 	host := infoTree.Get("host")
+	dbType := infoTree.Get("type")
 	maxOpenConnection := infoTree.Get("max_open_connection")
 	maxIdleConnection := infoTree.Get("max_idle_connection")
 	// Check if anything is missing.
 	if username == nil || password == nil || port == nil || host == nil{
-		fmt.Println("[Error]: Parameters missing! Please validate your .TOML file (Hint: Required parameters are: username, password, port, host)")
+		fmt.Println("[Error]: Parameters missing! Please validate your " +
+			".TOML file (Hint: Required parameters are: username, password, port, host)")
 		os.Exit(1)
 	}
+	db.dbType = dbType.(string)
 	db.username = username.(string)
 	db.password = password.(string)
+	db.database = database.(string)
 	db.host = host.(string)
 	db.port = port.(string)
 	mic := maxIdleConnection.(string)
@@ -54,15 +62,42 @@ func (db *Database) GetDatabaseDetails(env string) {
 
 // Connect into the database.
 func (db *Database) Connect() *sql.DB{
-	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", db.username, db.password, db.host, db.port, db.database)
-	DbCon, err := sql.Open("mysql", url)
+	url := ""
+
+	if db.dbType == "mysql"{
+		url = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", db.username, db.password, db.host, db.port, db.database)
+
+	}else if db.dbType == "postgres" {
+		url= fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", db.username, db.password, db.database)
+	}else{
+		fmt.Println("[Error]: Not supported database.")
+	}
+	DbCon, err := sql.Open(db.dbType, url)
 	if err != nil {
-		fmt.Println("Failed to connect database!")
+		fmt.Println(err)
 		os.Exit(1)
 	}
 	DbCon.Ping()
 	DbCon.SetMaxOpenConns(db.maxOpenConnection)
 	DbCon.SetMaxIdleConns(db.maxIdleConnection)
-	fmt.Println("[Status]: Connected!\nMaxOpenConnection:", db.maxOpenConnection, "\nMaxIdleConnection:", db.maxIdleConnection)
+	fmt.Println("[Status]: Connected!\nMaxOpenConnection:", db.maxOpenConnection,
+		    "\nMaxIdleConnection:", db.maxIdleConnection)
 	return DbCon
+}
+
+func main() {
+
+	connection := new(Database)
+	connection.GetDatabaseDetails("production")
+	cursor := connection.Connect()
+	defer cursor.Close()
+	rows, err := cursor.Query("SELECT CURRENT_DATE")
+	if err != nil{
+		fmt.Println(err)
+	}
+	for rows.Next() {
+		var time string
+		rows.Scan(&time)
+		fmt.Println(time)
+	}
 }
